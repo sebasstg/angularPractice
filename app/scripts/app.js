4d1e09b1dda456a127eea4961fff6d67e385829b
sebasstg <sebassalazart@gmail.com>
2016-08-22 00:46:17 -0500

    angular scope

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,54 +1,54 @@
 'use strict';
 angular.module('confusionApp', [])
 
-    //.controller('menuController', function() {
-    .controller('MenuController',['$scope', function($scope) {
+//.controller('menuController', function() {
+    .controller('MenuController', ['$scope', function ($scope) {
 
         // reemplzao this por scope
         $scope.tab = 1;
         $scope.filtText = '';
-        $scope.dishes=[
+        $scope.dishes = [
             {
-                name:'Uthapizza',
+                name: 'Uthapizza',
                 image: 'images/uthapizza.png',
                 category: 'mains',
-                label:'Hot',
-                price:'4.99',
-                description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+                label: 'Hot',
+                price: '4.99',
+                description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
                 comment: ''
             },
             {
-                name:'Zucchipakoda',
+                name: 'Zucchipakoda',
                 image: 'images/zucchipakoda.png',
                 category: 'appetizer',
-                label:'',
-                price:'1.99',
-                description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
+                label: '',
+                price: '1.99',
+                description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
                 comment: ''
             },
             {
-                name:'Vadonut',
+                name: 'Vadonut',
                 image: 'images/vadonut.png',
                 category: 'appetizer',
-                label:'New',
-                price:'1.99',
-                description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
+                label: 'New',
+                price: '1.99',
+                description: 'A quintessential ConFusion experience, is it a vada or is it a donut?',
                 comment: ''
             },
             {
-                name:'ElaiCheese Cake',
+                name: 'ElaiCheese Cake',
                 image: 'images/elaicheesecake.png',
                 category: 'dessert',
-                label:'',
-                price:'2.99',
-                description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
+                label: '',
+                price: '2.99',
+                description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
                 comment: ''
             }
         ];
 
-      //  $scope.dishes = dishes;
+        //  $scope.dishes = dishes;
 
-        $scope.select = function(setTab) {
+        $scope.select = function (setTab) {
             $scope.tab = setTab;
             if (setTab === 2) {
                 $scope.filtText = "appetizer";
@@ -67,8 +67,36 @@ angular.module('confusionApp', [])
             return ($scope.tab === checkTab);
         };
 
-        $scope.showDetails=false;
-        $scope.toggleDetails= function () {
+        $scope.showDetails = false;
+        $scope.toggleDetails = function () {
             $scope.showDetails = !$scope.showDetails;
+        };
+    }])
+
+    .controller('ContactController', ['$scope', function ($scope) {
+        $scope.feedback = {mychannel: "", firstName: "", lastName: "", agree: false, email: ""};
+        var channels = [{value: "tel", label: "Tel."}, {value: "Email", label: "Email"}];
+        $scope.channels = channels;
+        $scope.invalidChannelSelection = false;
+    }])
+    .controller('FeedbackController', ['$scope', function ($scope) {
+        $scope.sendFeedback = function () {
+            console.log($scope.feedback);
+            if ($scope.feedback.agree && ($scope.feedback.mychannel == "") && !$scope.feedback.mychannel) {
+
+                $scope.invalidChannelSelection = true;
+                console.log('incorrect');
+            }
+            else {
+                $scope.invalidChannelSelection = false;
+                $scope.feedback = {
+                    mychannel: "", firstName: "", lastName: "",
+                    agree: false, email: ""
+                };
+                $scope.feedback.mychannel = "";
+
+                $scope.feedbackForm.$setPristine();
+                console.log($scope.feedback);
+            }
         };
     }]);
